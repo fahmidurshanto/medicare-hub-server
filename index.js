515c5e6ab59bb2bc7bf63919a0790d298989aa44
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-06-14 21:48:16 +0600

    created mongdb connection

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clustercrud.ctitxen.mongodb.net/?retryWrites=true&w=majority&appName=clusterCrud`;
+
+// mongodb client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.use(cors());
 app.use(express.json());
 
-// API Home route
-app.get("/", (req, res) => {
-  res.send("Hello from Medicare Hub Server");
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running on port ${port}`);
-});
+    // API Home route
+    app.get("/", (req, res) => {
+      res.send("Hello from Medicare Hub Server");
+    });
+
+    // API listening to the port
+    app.listen(port, (req, res) => {
+      console.log(`Server is running on port ${port}`);
+    });
+    /*--------------------------------------------------------*/
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
